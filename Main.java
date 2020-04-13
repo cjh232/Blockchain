a5530e3ee0e005bd7468e90638182b59de12f244
cjh232 <46171942+cjh232@users.noreply.github.com>
2020-04-13 19:04:56 -0400

    Added miners for multithreading

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
 package blockchain;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Blockchain chain = Blockchain.getInstance();
 
-        for(int i = 0; i < 5; i++) {
-            chain.generateNextBlock();
+        ArrayList<Miner> minerList = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++) {
+            Miner miner = new Miner(i + 1);
+            minerList.add(miner);
+            miner.start();
         }
 
+        for(Miner miner: minerList) {
+            try {
+                miner.join();
+            }catch (Exception ex) {
+                System.out.println(String.format("Miner %d had an error joining", miner.getMinerID()));
+            }
+        }
+
+
         if(chain.validateChain()) {
-            chain.print();
+            // chain.print();
         }
     }
 }
