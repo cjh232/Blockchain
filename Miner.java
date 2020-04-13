92aafc5d6775400d95c4731327a996b99e530b26
cjh232 <46171942+cjh232@users.noreply.github.com>
2020-04-13 19:04:18 -0400

    Adjusted to simplify logic

--- Miner.java
+++ Miner.java
@@ -1,57 +1,78 @@
 package blockchain;
 
 import java.util.Date;
+import java.util.Random;
 
-public class Miner extends Thread{
+public class Miner extends Thread {
 
     private int minerID;
     private Blockchain blockchain;
 
-    public Miner(int id, Blockchain blockchain) {
-        this.blockchain = blockchain;
-        this.minerID = id;
+    public Miner(int minerID) {
+        this.minerID = minerID;
+        this.blockchain = Blockchain.getInstance();
     }
 
     @Override
     public void run() {
-        while(blockchain.size() < 6) {
-            mine(blockchain.getDifficulty());
+
+        for(int i = 0; i < 5; i++) {
+            mine(i + 1);
         }
     }
 
-    public void mine(int difficulty) {
-        Block lastBlock = blockchain.getTopBlock();
-        int id = blockchain.size();
+    public void mine(int blockNumber) {
+        Block lastBlock = blockchain.getLastBlock();
+        int prevID = lastBlock != null ? lastBlock.getId() : 0;
 
-        String previousHash = lastBlock.getCurrentHash();
-        int nonce = -1;
-        String currentHash;
+        // The miner is attempting to add a block that has already been placed.
+        if(blockNumber != prevID + 1) {
+            return;
+        }
 
-        String challengeString = "0".repeat(difficulty);
-        boolean hashFound;
-        String status;
-        long timeStamp = new Date().getTime();
-        
+        Random ran = new Random();
+
+        int currentDifficulty = blockchain.getDifficulty();
+
+        int id = lastBlock != null ? lastBlock.getId() + 1 : 1;
+        long timestamp = new Date().getTime();
+        int nonce = ran.nextInt() & Integer.MAX_VALUE;
+
+        String previousHash = lastBlock != null ? lastBlock.getCurrentHash() : "0";
+        String currentHash = generateHash(id, nonce, timestamp, previousHash);
+
+        String challenge = "0".repeat(currentDifficulty);
+        String subString = currentHash.substring(0, currentDifficulty);
+
+
         while(true) {
-            currentHash = generateHash(id, timeStamp, previousHash, ++nonce);
-            hashFound = currentHash.substring(0, difficulty).equals(challengeString);
 
-            if(hashFound) {
-                Block newBlock = new Block(minerID, id, timeStamp, previousHash, currentHash, nonce);
-                status = blockchain.addBlock(newBlock);
-
-                if(!status.equals("101")) {
+            if(subString.equals(challenge)) {
+                Block newBlock = new Block(minerID, id, nonce, timestamp, new Date().getTime(), previousHash, currentHash);
+                if(blockchain.offerBlock(newBlock, minerID)) {
                     return;
                 }
             }
+
+            lastBlock = blockchain.getLastBlock();
+            prevID = lastBlock != null ? lastBlock.getId() : 0;
+
+            if(id != prevID + 1) return;
+
+            nonce = ran.nextInt() & Integer.MAX_VALUE;
+            currentHash = generateHash(id, nonce, timestamp, previousHash);
+            subString = currentHash.substring(0, currentDifficulty);
+
         }
-
-
     }
 
-    private String generateHash(int id, long timestamp, String hashOfPreviousBlock, int nonce) {
-        String blockContents = "" + id + timestamp + hashOfPreviousBlock + nonce;
+    public int getMinerID() {
+        return minerID;
+    }
 
+    private String generateHash(int id, int nonce, long timestamp, String previousHash) {
+        String blockContents = id + nonce + timestamp + previousHash;
         return StringUtil.applySha256(blockContents);
     }
+
 }
