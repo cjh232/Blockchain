359a75080c5612c2fc3c557cac0180b642014b2f
cjh232 <46171942+cjh232@users.noreply.github.com>
2020-04-13 12:05:30 -0400

    Redesigned Project

--- Block.java
+++ Block.java
@@ -2,48 +2,54 @@ package blockchain;
 
 public class Block {
 
-    private int minerID;
     private int id;
+    private int nonce;
     private long timestamp;
+
     private String previousHash;
     private String currentHash;
-    private int nonce;
 
-    public Block(int minerID, int id, long timestamp, String previousHash, String currentHash, int nonce) {
-        this.minerID = minerID;
+    private long timeAdded;
+
+    public Block(int id, int nonce, long timestamp, long timeAdded, String previousHash, String currentHash) {
         this.id = id;
+        this.nonce = nonce;
         this.timestamp = timestamp;
+        this.timeAdded = timeAdded;
         this.previousHash = previousHash;
         this.currentHash = currentHash;
-        this.nonce = nonce;
-    }
-
-    public String getCurrentHash() {
-        return currentHash;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
     }
 
     public int getId() {
         return id;
     }
 
+    public long getTimestamp() {
+        return timestamp;
+    }
+
     public String getPreviousHash() {
         return previousHash;
+    }
+
+    public String getCurrentHash() {
+        return currentHash;
+    }
+
+    public long getComputeTime() {
+        return (timeAdded - timestamp) / 1000;
     }
 
     @Override
     public String toString() {
         return String.format("Block:\n" +
-                "Created by miner # %d\n" +
                 "Id: %d\n" +
                 "Timestamp: %d\n" +
                 "Magic number: %d\n" +
-                "Hash of the previous block:\n%s\n" +
-                "Hash of the block:\n%s\n"
-                , minerID, id, timestamp, nonce, previousHash, currentHash);
+                "Hash of the previous block:\n" +
+                "%s\n" +
+                "Hash of the block:\n" +
+                "%s\n" +
+                "Block was generating for %d seconds\n", id, timestamp, nonce, previousHash, currentHash, getComputeTime());
     }
-
 }
