e8c116bb475b923c97450e6f6ff8ff0f0beb6d0c
cjh232 <46171942+cjh232@users.noreply.github.com>
2020-04-13 12:06:58 -0400

    Redesigned Project

--- Main.java
+++ Main.java
@@ -1,27 +1,15 @@
 package blockchain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
+        Blockchain chain = Blockchain.getInstance();
 
-        Blockchain bc =  Blockchain.getInstance();
-        List<Miner> threadList = new ArrayList<>();
-
-        for(int i = 0; i < 50; i++) {
-            Miner miner = new Miner(i + 1, bc);
-            threadList.add(miner);
-            miner.start();
+        for(int i = 0; i < 5; i++) {
+            chain.generateNextBlock();
         }
 
-        for(Miner miner: threadList) {
-            try {
-                miner.join();
-            } catch (Exception e) {
-                System.out.println("Error Occurred");
-            }
+        if(chain.validateChain()) {
+            chain.print();
         }
-
     }
 }
